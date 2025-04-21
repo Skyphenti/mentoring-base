51fe1e2295b3f3d2b4420cb2788753173891fc4d
Skyphenti <oleg260197@gmail.com>
2025-04-21 12:39:17 +0300

    feat function buttons and areas of classes
    1. Добавлены кнопка, изменяющая регистр меню и скрывающая баннер

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,69 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { Component, numberAttribute } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+
+  readonly headerNavItem1 = 'Главная';
+  readonly headerNavItem2 = 'О компании';
+  readonly headerNavItem3 = 'Каталог';
+
+  headerPhoneNumber = '+7 (965) 084-29-29';
+
+  readonly menuItemCatalogTitle = 'Каталог';
+
+  heroText = 'Инструмент для вашего бизнеса';
+
+  featuresItem1MainText = 'Большой ассортимент';
+  featuresItem2MainText = 'Доставка стройматериалов';
+  featuresItem3MainText = 'система управления качеством';
+
+  featuresItem1SecondaryText = 'строительных материалов всегда в наличии';
+  featuresItem2SecondaryText = 'в течение четырех часов или в любое удобное время'
+  featuresItem3SecondaryText1 = 'Многоуровневая'
+  featuresItem3SecondaryText2 = 'по стандарту ISO 9001'
+
+  isCatalogShow = false;
+
+  isHeroShow = true;
+
+  readonly aboutCompany = navMenuItemName;
+
+  newPages = newPages;
+
+  isUpperCase = true;
+  
+  menuItems = upperCaseMenuItems
+  
+  changeMenuText() {
+    this.menuItems = menuItems.map(
+      item => this.isUpperCase ? item.toLowerCase() : item.toUpperCase()
+    )
+    
+    this.isUpperCase = !this.isUpperCase
+  }
+  
 }
+
+function navMenu(navMenuItem: string) {
+  return navMenuItem
+}
+const menuItems = ['Стройматериалы' , 'Инструменты', 'Электрика', 'Интерьер и отделка'];
+const upperCaseMenuItems = menuItems.map(item => item.toUpperCase())
+
+const navItemName: string = 'О компании';
+
+const navMenuItemName = navMenu(navItemName)
+
+const newPages = [5,4,3,2,1]
+
+
+
